00aab9c287f90c0c95bf6b5e1f7a5fc4c838d944
pure-js <evolution.is.already.underway@gmail.com>
2017-01-13 17:22:13 +0300

    add item component

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ItemComponent } from './item.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ItemComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'item',
+  templateUrl: 'app/item.component.html'
+})
+
+export class ItemComponent {
+  kik = 23;
+  oli = 434;
+}

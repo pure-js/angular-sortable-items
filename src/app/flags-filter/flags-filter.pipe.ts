2d8d5042cdc3e5a1d7a8e6da69decd613a93df0f
pure-js <evolution.is.already.underway@gmail.com>
2017-01-24 19:23:50 +0300

    fix

--- src/app/flags-filter/flags-filter.pipe.ts
+++ src/app/flags-filter/flags-filter.pipe.ts
@@ -9,34 +9,12 @@ import { Item } from '../item';
 export class FlagPipe implements PipeTransform {
   transform(arr: Item[], flags: string[]) {
 
-    // let findOne = function (haystack: any, arr: any) {
-    //   return arr.every(function (element: any) {
-    //     // console.log('el', element);
-    //     return haystack.indexOf(element) >= 0;
-    //   });
-    // };
-    //
-    // return arr.filter((value) => {
-    //   return findOne(flags, value.flags)
-    // });
-
-    function findEachFlag(value: Item) {
-      console.log('f', flags);
-      // value.flags;
-
-      function hasAllProps(value: string) {
-        for (let i = 0; i < flags.length; i++) {
-          console.log(flags[i]);
-          console.log('separator');
-          let result = value.indexOf(flags[i]) >= 0;
-          if(!result) {
-            return false
-          }
-        }
-        return true;
+    function findEachFlag(value: any) {
+      function hasProperty(val: string) {
+        return value.flags.includes(val);
       }
 
-      return value.flags.every(hasAllProps);
+      return flags.every(hasProperty);
     }
 
     return arr.filter(findEachFlag);

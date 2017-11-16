10f9abe295d5e1e4179ff15c8f7dd6674fe74eea
pure-js <evolution.is.already.underway@gmail.com>
2017-11-16 20:04:18 +0300

    code style fixes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
+import { AppComponent } from './app.component';
 
 import { AlphabeticalOrderPipe } from './alphabetical-order.pipe';
 import { FlagsFilterComponent } from './flags-filter/flags-filter.component';
@@ -13,6 +15,8 @@ import { TextFilterComponent } from './text-filter/text-filter.component';
 import { TextFilterPipe } from './text-filter.pipe';
 import { FlagsFilterPipe } from './flags-filter.pipe';
 
+import { InMemoryDataService } from './in-memory-data.service';
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -20,6 +24,8 @@ describe('AppComponent', () => {
       imports: [
         HttpClientModule,
         FormsModule,
+        BrowserModule,
+        HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService),
       ],
       declarations: [
         AppComponent,
@@ -46,10 +52,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Angular sortable items');
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a h1 tag', async((done) => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('label').textContent).toContain('Sort a-z:');
+    expect(compiled.querySelector('label').textContent).toEqual('Sort a-z:');
   }));
 });
